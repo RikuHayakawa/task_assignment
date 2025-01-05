2a6f1a3e228d20108ed2a43c8adfa98eadddd69e
RikuHayakawa <ai20ka18wa@outlook.jp>
2025-01-05 18:57:01 +0900

    update: ScheduleOption

--- include/schedule_option.h
+++ include/schedule_option.h
@@ -1,22 +1,25 @@
-#ifndef CCHARGING_SCHEDULE_H
-#define CCHARGING_SCHEDULE_H
+#ifndef SCHEDULE_OPTION_H
+#define SCHEDULE_OPTION_H
 #include <map>
 #include <vector>
 #include <string>
 
-namespace schedule_option
+namespace schedule_planner
 {
     class ScheduleOption
     {
     public:
-        int id;
         int robot_id;
         bool isDecided = false;
-        std::vector<std::vector<int>> binary_option;
+        std::vector<int> binary_option;
         std::vector<std::pair<std::string, int>> m_assignment;
 
         void setIsDecided(bool status) { isDecided = status; }
-        static ScheduleOption createScheduleOptions(const std::vector<int> &taskIds, const int robot_id, const int constantTime);
+
+    public:
+        ScheduleOption();
+        ScheduleOption(const std::vector<std::pair<std::string, int>> &assignment, const int robot_id, const bool isDecided, std::vector<int> &binary_option);
+        ~ScheduleOption();
     };
 
 }
