70ed6a99a49e80d4312dbb924e54149c5c4b4e3c
RikuHayakawa <ai20ka18wa@outlook.jp>
2025-01-18 23:28:22 +0900

    update: 実験のための出力

--- include/exec_result.h
+++ include/exec_result.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <gap.h>
 
 namespace exec_result
 {
@@ -26,20 +27,19 @@ namespace exec_result
         // モデル内で計算
         std::tuple<int, double, double, double, int> m_initial_result;   // 初期結果(競合数, 標準偏差, 評価値, 平均収容率, 最大収容数)
         std::tuple<int, double, double, double, int> m_scheduled_result; // スケジュール結果(競合数, 標準偏差, 評価値, 平均収容率, 最大収容数)
-        int m_assigned_tasks;                                            // 割り当てられた総タスク数
-        int m_assigned_chargings;                                        // 充電を行うロボット数
-        double m_average_idle_time;                                      // 充電またはタスクを行っていない時間の平均
-        bool m_is_poor_charge;                                           // 実行終了時の最小充電残量が閾値を超えているかのフラグ
-        double m_finishing_average_charge;                               // 実行終了時の平均充電残量
-        double m_average_occupation_rate;                                // 平均収容率
-        int m_max_capacity;                                              // 最大収容数
+        int m_assigned_tasks = 0;                                        // 割り当てられた総タスク数
+        double m_initial_charging_average = 0;                           // 充電計画前の平均充電回数
+        double m_scheduled_charging_average = 0;                         // 充電計画後の平均充電回数
+        int m_charging_robot_num = 0;                                    // 充電が割り当てられたロボット数
+        double rest_energy_average = 0;                                  // 充電残量の平均
 
         void calculateStationOccupation(std::vector<int> occupation_per_time, std::tuple<int, double, double, double, int> &result);
 
     public:
         ExecResult();
         ExecResult(std::string test_id, int total_tasks, int total_robots, int constraint_time, int guaranteed_energy, int station_capacity,
-                   double gap_for_constraint_size_time, double gap_for_constraint_time, double schedule_planner_time, double exec_time, std::vector<int> initial_occupation, std::vector<int> schedule_occupation);
+                   double gap_for_constraint_size_time, double gap_for_constraint_time, double schedule_planner_time, double exec_time, std::vector<int> initial_occupation,
+                   std::vector<int> schedule_occupation, gap::CGap *gap_instance);
         virtual ~ExecResult();
         void Print();
     };
--- src/exec_result.cpp
+++ src/exec_result.cpp
@@ -5,6 +5,7 @@
 #include <exec_result.h>
 #include <algorithm>
 #include <schedule_planner.h>
+#include <gap.h>
 
 namespace exec_result
 {
@@ -12,13 +13,83 @@ namespace exec_result
     {
     }
     ExecResult::ExecResult(std::string test_id, int total_tasks, int total_robots, int constraint_time, int guaranteed_energy, int station_capacity,
-                           double gap_for_constraint_size_time, double gap_for_constraint_time, double schedule_planner_time, double exec_time, std::vector<int> initial_occupation, std::vector<int> schedule_occupation)
+                           double gap_for_constraint_size_time, double gap_for_constraint_time, double schedule_planner_time, double exec_time, std::vector<int> initial_occupation, std::vector<int> schedule_occupation, gap::CGap *gap_instance)
         : m_test_id(test_id), m_total_tasks(total_tasks), m_total_robots(total_robots), m_constraint_time(constraint_time), m_guaranteed_energy(guaranteed_energy), m_station_capacity(station_capacity),
           m_gap_for_constraint_size_time(gap_for_constraint_size_time), m_gap_for_constraint_time(gap_for_constraint_time), m_schedule_planner_time(schedule_planner_time), m_exec_time(exec_time),
           m_initial_occupation(initial_occupation), m_schedule_occupation(schedule_occupation)
     {
         calculateStationOccupation(initial_occupation, m_initial_result);
         calculateStationOccupation(schedule_occupation, m_scheduled_result);
+
+        int checkChargingRobot = 0;
+        int idleTime = gap_instance->m_bins.size() * gap_instance->constaint_time;
+
+        for (int i = 0; i < gap_instance->m_bins.size(); i++)
+        {
+            const int initialAssignmentSize = gap_instance->m_bins[i].m_initial_assignment.m_assignment.size();
+            const int scheduledAssignmentSize = gap_instance->m_bins[i].m_scheduled_assignment.m_assignment.size();
+            gap_instance->m_bins[i].m_initial_assignment.removeChargeAssignment();
+            const int initialTaskSize = gap_instance->m_bins[i].m_initial_assignment.m_assignment.size();
+            gap_instance->m_bins[i].m_scheduled_assignment.removeChargeAssignment();
+            const int scheduledTaskSize = gap_instance->m_bins[i].m_scheduled_assignment.m_assignment.size();
+
+            if (initialAssignmentSize > initialTaskSize)
+            {
+                checkChargingRobot++;
+                // 計画前の充電数をカウント
+                m_initial_charging_average += initialAssignmentSize - initialTaskSize;
+            }
+            if (scheduledAssignmentSize > scheduledTaskSize)
+            {
+                // 充電が割り当てられたロボット数をカウント
+                m_charging_robot_num++;
+                // 計画後の充電数をカウント
+                m_scheduled_charging_average += scheduledAssignmentSize - scheduledTaskSize;
+            }
+            if (initialTaskSize != scheduledTaskSize)
+            {
+                // タスク数が異なる場合エラー
+                cerr << "Error: Initial assignment and scheduled assignment are different." << endl;
+                exit(1);
+            }
+            if (checkChargingRobot != m_charging_robot_num)
+            {
+                // 充電を行うロボット数が異なる場合エラー
+                cerr << "Error: Charging robot number is different." << endl;
+                exit(1);
+            }
+            // 割り当てられたタスク数をカウント
+            m_assigned_tasks += scheduledTaskSize;
+            if (gap_instance->m_bins[i].m_scheduled_assignment.m_total_time == gap_instance->m_bins[i].m_initial_assignment.m_total_time)
+            {
+                idleTime -= gap_instance->m_bins[i].m_scheduled_assignment.m_total_time;
+            }
+            else
+            {
+                // ロボットの総稼働時間が異なる場合エラー
+                cerr << "Error: Total time is different." << endl;
+            }
+
+            // 充電残量の平均を算出
+            rest_energy_average += gap_instance->m_bins[i].m_scheduled_assignment.m_rest_energy;
+
+            if (gap_instance->m_bins[i].m_scheduled_assignment.m_rest_energy < gap_instance->m_guaranteed_energy)
+            {
+                // error: 最小充電残量が閾値を超えている場合
+                cerr << "Error: Poor charge." << endl;
+                exit(1);
+            }
+        }
+        m_initial_charging_average /= gap_instance->m_bins.size();
+        m_scheduled_charging_average /= gap_instance->m_bins.size();
+        rest_energy_average /= gap_instance->m_bins.size();
+
+        if (idleTime != 0)
+        {
+            // 充電またはタスクを行わないロボットが存在する場合エラー
+            cerr << "Error: Idle time is not 0." << endl;
+            exit(1);
+        }
     }
     ExecResult::~ExecResult()
     {
@@ -65,5 +136,11 @@ namespace exec_result
         std::cout << std::endl;
         std::cout << "Initial result: " << "競合数" << std::get<0>(m_initial_result) << " 標準偏差" << std::get<1>(m_initial_result) << " 評価値" << std::get<2>(m_initial_result) << " 平均収容率" << std::get<3>(m_initial_result) << " 最大収容数" << std::get<4>(m_initial_result) << std::endl;
         std::cout << "Scheduled result: " << "競合数" << std::get<0>(m_scheduled_result) << " 標準偏差" << std::get<1>(m_scheduled_result) << " 評価値" << std::get<2>(m_scheduled_result) << " 平均収容率" << std::get<3>(m_scheduled_result) << " 最大収容数" << std::get<4>(m_scheduled_result) << std::endl;
+
+        std::cout << "Assigned tasks: " << m_assigned_tasks << std::endl;
+        std::cout << "Initial charging average: " << m_initial_charging_average << std::endl;
+        std::cout << "Scheduled charging average: " << m_scheduled_charging_average << std::endl;
+        std::cout << "Charging robot num: " << m_charging_robot_num << std::endl;
+        std::cout << "Rest energy average: " << rest_energy_average << std::endl;
     }
 }
